2e02ab6e5cbdff6e3be0614d31188243e5cb78b9
Zyris Shakir <zeyeland@gmail.com>
2018-05-18 13:37:38 -0500

    Generated and routed user create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 // 2. Import the UserComponent
 import { UsersComponent }   from './users/users.component';
 import { UserViewComponent } from './user-view/user-view.component';
+import { UserCreateComponent } from './user-create/user-create.component';
 
 // 3. Declare your routes
 const routes: Routes = [
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   // 5. Map /users to the UsersComponent
   { path: 'users', component: UsersComponent },
-  { path: 'users/view/:id', component: UserViewComponent }
+  { path: 'users/view/:id', component: UserViewComponent },
+  { path: 'users/create', component: UserCreateComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { UsersComponent } from './users/users.component';
 import {UserService} from './user.service';
 import { AppRoutingModule } from './/app-routing.module';
 import { UserViewComponent } from './user-view/user-view.component';
+import { UserCreateComponent } from './user-create/user-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserViewComponent
+    UserViewComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule,
